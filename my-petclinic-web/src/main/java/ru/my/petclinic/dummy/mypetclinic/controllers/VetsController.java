d9ac74522197c5d7d0325dfb606193fcd6a19797
oandreev <oandreev@cinimex.ru>
2019-05-14 23:15:44 +0300

    90. Spring Pet Clinic - List Vets: Доработан VetController and index.html

--- my-petclinic-web/src/main/java/ru/my/petclinic/dummy/mypetclinic/controllers/VetsController.java
+++ my-petclinic-web/src/main/java/ru/my/petclinic/dummy/mypetclinic/controllers/VetsController.java
@@ -1,13 +1,23 @@
 package ru.my.petclinic.dummy.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.my.petclinic.dummy.mypetclinic.services.VetService;
 
 @Controller
 public class VetsController {
 
+    private VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
