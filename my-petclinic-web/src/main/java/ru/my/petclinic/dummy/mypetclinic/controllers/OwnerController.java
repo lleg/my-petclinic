910be44c7c698eb160f1780e2d4e498312c0224e
oandreev <oandreev@cinimex.ru>
2019-05-14 23:07:18 +0300

    89. Spring Pet Clinic - List Owners: Доработан OwnerController и index.html

--- my-petclinic-web/src/main/java/ru/my/petclinic/dummy/mypetclinic/controllers/OwnerController.java
+++ my-petclinic-web/src/main/java/ru/my/petclinic/dummy/mypetclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package ru.my.petclinic.dummy.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.my.petclinic.dummy.mypetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "/owners/index";
     }
 }
