df383cd6df641484c1ecfa4a59f155788768aa60
oandreev <oandreev@cinimex.ru>
2019-05-26 23:44:03 +0300

    126 -  add Specialty entity

---
+++ my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/model/Specialty.java
@@ -0,0 +1,14 @@
+package ru.my.petclinic.dummy.mypetclinic.model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/model/Vet.java
+++ my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package ru.my.petclinic.dummy.mypetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends  Person {
+
+    private Set<Specialty> specialty;
+
+    public Set<Specialty> getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Set<Specialty> specialty) {
+        this.specialty = specialty;
+    }
 }
