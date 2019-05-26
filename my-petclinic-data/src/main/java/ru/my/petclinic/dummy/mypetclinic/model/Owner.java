e3253eecbef2e693ccde52718aff9007499b2f09
oandreev <oandreev@cinimex.ru>
2019-05-26 23:37:57 +0300

    125 -  add Visits entity

--- my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/model/Owner.java
+++ my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/model/Owner.java
@@ -1,4 +1,20 @@
 package ru.my.petclinic.dummy.mypetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+
+
+
 }
