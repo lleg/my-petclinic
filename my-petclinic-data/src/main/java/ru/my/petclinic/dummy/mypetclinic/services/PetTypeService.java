65820cf565f24fd1130f6e2fce829c7d90bac80b
oandreev <oandreev@cinimex.ru>
2019-05-27 22:51:58 +0300

    Revert "128-129  -  add PetType"
    This reverts commit 48eb5acb

--- my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/services/PetTypeService.java
+++
@@ -1,6 +0,0 @@
-package ru.my.petclinic.dummy.mypetclinic.services;
-
-import ru.my.petclinic.dummy.mypetclinic.model.PetType;
-
-public interface PetTypeService extends CrudService<PetType, Long> {
-}
