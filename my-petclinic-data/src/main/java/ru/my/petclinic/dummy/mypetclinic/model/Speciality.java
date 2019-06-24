426d853cfeb439a6d87e506fa8d211f1fbbcb6d4
oandreev <oandreev@cinimex.ru>
2019-06-24 19:54:07 +0300

    154 - Добавлен JPA маппинг Vet and Specialities

--- my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/model/Speciality.java
+++ my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package ru.my.petclinic.dummy.mypetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/model/Vet.java
+++ my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/model/Vet.java
@@ -1,10 +1,16 @@
 package ru.my.petclinic.dummy.mypetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends  Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vats_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+     inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> speciality = new HashSet<>();
 
     public Set<Speciality> getSpeciality() {
