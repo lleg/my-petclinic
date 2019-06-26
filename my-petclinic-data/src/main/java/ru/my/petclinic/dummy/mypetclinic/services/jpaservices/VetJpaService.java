43c5f78361e690e31aa16af56d61007004bb4b2f
oandreev <oandreev@cinimex.ru>
2019-06-26 23:22:31 +0300

    159 - Реализован VetJpaService

---
+++ my-petclinic-data/src/main/java/ru/my/petclinic/dummy/mypetclinic/services/jpaservices/VetJpaService.java
@@ -0,0 +1,49 @@
+package ru.my.petclinic.dummy.mypetclinic.services.jpaservices;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import ru.my.petclinic.dummy.mypetclinic.model.Vet;
+import ru.my.petclinic.dummy.mypetclinic.repository.VetRepository;
+import ru.my.petclinic.dummy.mypetclinic.services.VetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpaservices")
+public class VetJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
